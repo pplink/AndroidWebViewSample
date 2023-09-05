5aa1c63b1784d67ffa37556a545bf696d6d0f3ef
Jaeseong Seo <07js23@gmail.com>
2023-09-05 11:35:33 +0900

    Minimum pagecall example

--- app/src/main/java/com/example/androidwebviewsample/MainActivity.kt
+++ app/src/main/java/com/example/androidwebviewsample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidwebviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById < WebView >(R.id.webview);
+        webView.settings.javaScriptEnabled = true;
+        webView.loadUrl("https://app.pagecall.com/meet?room_id=64f693ae93ac39a38ba5ce9a");
     }
 }
